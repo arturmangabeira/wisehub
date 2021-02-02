982f0c62b3d6bc7842a335e28c8f428fc03995ab
Artur Mangabeira <arturmangabeira@gmail.com>
2021-02-02 16:26:26 -0300

    pull teste

--- app/Candidate.php
+++ app/Candidate.php
@@ -13,4 +13,9 @@ class Candidate extends Model
     public function user(){
         return $this->belongsTo(User::class);
     }
+
+    public function analisar()
+    {
+        return "teste";
+    }
 }
